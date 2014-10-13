06df89fab8df976e945c3a2b39a958ebba8dc0f7
Finlay Maguire <finlaymaguire@gmail.com>
2014-10-13 16:23:15 +0100

    update page gen script to capital case title correctly

--- page_gen_from_md.sh
+++ page_gen_from_md.sh
@@ -14,13 +14,11 @@ SED_CMD="/<section>/r $temp_html_file"
 sed -e $SED_CMD template.html > ${output_file}
 
 name_with_spaces=${input_name//_/ }
+name_with_spaces_and_caps=$(echo $name_with_spaces | sed -e 's/^./\U&/g; s/ ./\U&/g')
 name_with_dashes=${input_name//_/-}
 
-title_string="<h2><a name=\"${name_with_dashes}\" class=\"anchor\" href=\"#${name_with_dashes}\"><span class=\"octicon octicon-link\"></span></a>${name_with_spaces}</h2>"
-echo $title_string
+title_string="<h2><a name=\"${name_with_dashes}\" class=\"anchor\" href=\"#${name_with_dashes}\"><span class=\"octicon octicon-link\"></span></a>${name_with_spaces_and_caps}</h2>"
+
 sed -i "/<section>/a ${title_string}" ${output_file}
 
 rm $temp_html_file 
-
-
-
